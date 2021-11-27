0438dcc05a08db00cbb57fd631309c4ca3dc326d
benjs <hello@benjs.de>
2021-11-27 16:22:12 +0100

    Custom shell files

---
+++ custom/slurm_train.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#SBATCH --job-name=uORF
+#SBATCH --ntasks=1
+#SBATCH --time=48:00:00
+
+# Number of gpus. Available nodes: dev_gpu_4, gpu_4, gpu_8
+#SBATCH --gres=gpu:4
+#SBATCH --partition=gpu_8
+
+# Output files
+#SBATCH --output=../out/R-%x.%j.out
+#SBATCH --error=../out/R-%x.%j.err
+
+# 216:00:00
+# 48:00:00
+# 00:30:00
+
+echo 'Activate conda environment'
+source ~/.bashrc
+conda activate uorf-3090
+
+echo 'Start training'
+exec custom/train_room_diverse.sh
